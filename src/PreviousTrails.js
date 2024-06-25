ea25ec62b592d08f8b2c03292e28bc2bf88b9392
Alexis12119 <corporal461@gmail.com>
2024-06-26 01:04:27 +0800

    test

--- src/PreviousTrails.js
+++ src/PreviousTrails.js
@@ -68,7 +68,7 @@ const PreviousTrails = ({ trails, fetchTrails, handleTrailSelect }) => {
 
   return (
     <div className="w-full h-full p-4 bg-gray-100 ">
-      <div className="top-0 bg-gray-100 z-10">
+      <div className="sticky top-0 bg-gray-100 z-10">
         <select
           className="mb-4 p-2 rounded border w-full"
           value={sortOrder}
